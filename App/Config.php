7be89db6d06e618e121bca863cc7c71e7319df38
Ahmad Imtiaz Shamim <emtiaz51921@gmail.com>
2021-09-19 11:26:10 +0600

    Update config.php
    Change credentials to heroku

--- App/Config.php
+++ App/Config.php
@@ -10,23 +10,23 @@ class Config {
      * Database Host
      * @var String
      */
-    const DB_HOST = '127.0.0.1';
+    const DB_HOST = 'ec2-54-146-84-101.compute-1.amazonaws.com';
 
     /**
      * Database Name
      * @var String
      */
-    const DB_NAME = 'oop_crud';
+    const DB_NAME = 'd8990qetdlk30k';
 
     /**
      * Database User
      * @var String
      */
-    const DB_USER = 'root';
+    const DB_USER = 'ugnygqiupgarep';
 
     /**
      * Database Password
      * @var String
      */
-    const DB_PASSWORD = 'Odesk121!';
+    const DB_PASSWORD = '05f671654fb79191ebd264ddaf26c138c46c1d96a127aed333afed20eda9d10f';
 }
